d8f564fbbd13ab3c70e4b688b484c8e48d0eb5a1
Luuk Domhof <luukdomhof@exp-plus.nl>
2021-08-22 00:32:27 +0200

    add Level 2 and Failsafe

--- Assets/Scripts/Managers/PlayerManager.cs
+++ Assets/Scripts/Managers/PlayerManager.cs
@@ -1,16 +1,19 @@
 using UnityEngine;
+using ExpPlus.Phariables;
 
 public class PlayerManager : MonoBehaviour
 {
+    [Header("References"),SerializeField]
+    private FloatPhariable failSafeDepth;
 
-    private void OnEnable()
+    [Header("Events"),SerializeField]
+    private BoolPhariable resetGame;
+
+    private void Update()
     {
-        
+        if(transform.position.y < failSafeDepth.value)
+        {
+            resetGame.value = true;
+        }
     }
-
-    private void OnDisable()
-    {
-
-    }
-
 }
